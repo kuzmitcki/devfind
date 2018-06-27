977788a51932a4b098b9411e6d26ddfa28be2d84
kuzmitski <kuzmitskigd@gmail.com>
2018-06-27 17:41:22 +0300

    commit

--- src/main/java/com/intinctools/controllers/TestController.java
+++ src/main/java/com/intinctools/controllers/TestController.java
@@ -1,22 +1,10 @@
 package com.intinctools.controllers;
 
 
-import com.intinctools.entities.Role;
-import com.intinctools.entities.User;
-import com.intinctools.entities.WorkDay;
-import com.intinctools.repo.SpecializationRepo;
-import com.intinctools.repo.UserRepo;
-import com.intinctools.repo.WorkDayRepo;
-import jdk.nashorn.internal.runtime.Specialization;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.sql.Time;
-import java.util.Collections;
 
 
 @Controller
